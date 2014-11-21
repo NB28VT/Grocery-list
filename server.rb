d25d2d82daf927dd5a6b451260cf0a5378d9b72a
Nathan Burgess <nathan.burgess28@hotmail.com>
2014-11-21 09:39:06 -0500

    Completed assignment. Need to add CSS styling for fun

--- server.rb
+++ server.rb
@@ -6,10 +6,9 @@ require 'sinatra/reloader'
 
 
 
-def write_to_file
-
-  File.open("some-file.txt", "a") do |f|
-    f.write("blah\n")
+def write_to_file(item)
+  File.open("grocery_list.txt", "a") do |f|
+    f.write("#{item}\n")
   end
 end
 
@@ -17,11 +16,16 @@ end
 def read_from_file(file)
   grocery_list = File.read(file)
   grocery_list
-  binding.pry
 end
 
 
 get '/groceries' do
   @grocery_list = read_from_file("grocery_list.txt")
   erb :index
+end
+
+post '/groceries' do
+  @item = params[:item]
+  write_to_file(@item)
+  redirect '/groceries'
 end
